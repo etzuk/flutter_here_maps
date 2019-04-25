489f72440a58faf9e1c9fcd9cbfe6cb2daff1e0a
Itzhak <itzhak.gueta@here.com>
2019-04-25 07:14:06 +0300

    Update readme

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -8,10 +8,13 @@ import NMAKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
+    let appID = ""
+    let appCode = ""
+    let licenseKey = ""
 
-    let error =  NMAApplicationContext.setAppId(kHelloMapAppID,
-                                                appCode: kHelloMapAppCode,
-                                                licenseKey: kHelloMapLicenseKey)
+    let error =  NMAApplicationContext.setAppId(appID,
+                                                appCode: appCode,
+                                                licenseKey: licenseKey)
     assert(error == NMAApplicationContextError.none)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
