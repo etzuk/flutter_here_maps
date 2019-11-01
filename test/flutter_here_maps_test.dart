7b80a9e78114bbe948ca49f04a86f4f174913d8a
etzuk <etzukg@gmail.com>
2019-11-01 08:49:28 +0200

    Update flutter SDK

--- test/flutter_here_maps_test.dart
+++ test/flutter_here_maps_test.dart
@@ -5,7 +5,6 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_here_maps/gen/map_objects.pb.dart';
 import 'package:flutter_test/flutter_test.dart';
 
