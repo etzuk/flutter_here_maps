90b7b0024a81c6d5a2dd2c7f70eb6bdc8779c3eb
itzhak.gueta <itzhak.gueta@here.com>
2020-01-16 11:15:12 +0200

    Enable pass initital map state

--- lib/map/map_view.dart
+++ lib/map/map_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_here_maps/flutter_here_maps.dart';
+import 'package:flutter_here_maps/proto_gen/map_objects.pb.dart';
 
 typedef void MapCreatedCallback(HereMapsController controller);
 
@@ -11,8 +12,10 @@ class MapView extends StatefulWidget {
   const MapView({
     Key key,
     this.onMapCreated,
+    this.initialMapCenter,
   }) : super(key: key);
   final MapCreatedCallback onMapCreated;
+  final MapCenter initialMapCenter;
   @override
   State<StatefulWidget> createState() {
     return _MapViewState();
@@ -24,16 +27,25 @@ class _MapViewState extends State<MapView> {
       Completer<HereMapsController>();
   @override
   Widget build(BuildContext context) {
+    final Map<String, String> creationParams = <String, String>{
+      'initialMapCenter': widget.initialMapCenter.writeToJson(),
+    };
+    print(creationParams);
     if (defaultTargetPlatform == TargetPlatform.android) {
       return AndroidView(
-          viewType: 'flugins.etzuk.flutter_here_maps/MapView',
-          onPlatformViewCreated: onPlatformViewCreated,
-          creationParamsCodec: const StandardMessageCodec());
+        viewType: 'flugins.etzuk.flutter_here_maps/MapView',
+        onPlatformViewCreated: onPlatformViewCreated,
+        creationParams:
+            ByteData.view(widget.initialMapCenter.writeToBuffer().buffer),
+        creationParamsCodec: const BinaryCodec(),
+      );
     } else {
       return UiKitView(
           viewType: 'flugins.etzuk.flutter_here_maps/MapView',
           onPlatformViewCreated: onPlatformViewCreated,
-          creationParamsCodec: const StandardMessageCodec());
+          creationParams:
+              ByteData.view(widget.initialMapCenter.writeToBuffer().buffer),
+          creationParamsCodec: const BinaryCodec());
     }
   }
 
