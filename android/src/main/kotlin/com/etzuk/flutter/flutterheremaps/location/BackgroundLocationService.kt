d9245b6540f778f1f7ff15187226cfd216da5379
itzhak.gueta <itzhak.gueta@here.com>
2020-02-06 11:08:25 +0200

    Start wakelock when foreground service is created TODO: Pass the timeout to acquire()

--- android/src/main/kotlin/com/etzuk/flutter/flutterheremaps/location/BackgroundLocationService.kt
+++ android/src/main/kotlin/com/etzuk/flutter/flutterheremaps/location/BackgroundLocationService.kt
@@ -5,9 +5,10 @@ import FlutterHereMaps.MapObjects
 import android.app.*
 import android.content.Context
 import android.content.Intent
-import android.os.Binder
+
 import android.os.Build
 import android.os.IBinder
+import android.os.PowerManager
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
@@ -28,12 +29,19 @@ class BackgroundLocationService : Service(), PositioningManager.OnPositionChange
         const val BROADCAST_ACTION = "Location_Broadcast_Actions"
         const val EXTRA_DATA = "Location_Read_Data"
         const val EXTRA_SETTINGS = "Location_Settings"
+        const val WAKE_LOCK_TAG = "BackgroundLocationService::WakeLock"
 
     }
 
 
     override fun onCreate() {
         super.onCreate()
+        (getSystemService(Context.POWER_SERVICE) as PowerManager)
+                .run {
+                    newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, WAKE_LOCK_TAG).apply {
+                        acquire()
+                    }
+                }
         localBroadcastReceiver = LocalBroadcastManager.getInstance(this)
     }
 
@@ -117,7 +125,14 @@ class BackgroundLocationService : Service(), PositioningManager.OnPositionChange
             positioningManager.stop()
             positioningManager.removeListener(this)
         }
-
+        (getSystemService(Context.POWER_SERVICE) as PowerManager).run {
+            newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, WAKE_LOCK_TAG).apply {
+                if (isHeld) {
+                    setReferenceCounted(false)
+                    release()
+                }
+            }
+        }
         stopForeground(true)
         stopSelf()
     }
